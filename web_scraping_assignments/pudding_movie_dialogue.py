1ec6e76495efae4bfb1ed1fda4688f6a45e065b7
root <root@LAPTOP-3EH4VBDS>
2024-02-20 04:36:50 -0600

    add

--- web_scraping_assignments/pudding_movie_dialogue.py
+++ web_scraping_assignments/pudding_movie_dialogue.py
@@ -2,17 +2,16 @@ import requests
 import pandas as pd
 import csv
 from bs4 import BeautifulSoup
+
 df = pd.read_csv('cleaned_pudding_data.csv')
 links = df['link']
 with open('pudding_movie_dialogue.csv', 'w') as file:
      writer = csv.writer(file)
      writer.writerow(['Link', 'Script'])
-     for link in links:
-            response = requests.get(link)
+     for i in links:
+            response = requests.get(i)
             if response.status_code != 200:
                         continue
             soup = BeautifulSoup(response.text, "html.parser")
-            soup.prettify()
-            script = soup.get_text()[:1000]
-            # print(script)
-            writer.writerow([link, script])
+            writer.writerow([ i, soup.get_text()[:1000]])
+
